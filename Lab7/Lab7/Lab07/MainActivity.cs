6f93356be448ddb666ed06f3364b21ad2fb51a19
dacanetdev <iscdavidcarrillo@gmail.com>
2017-06-29 04:40:49 -0400

    Updated Lab 7
    Added Lab07 code

--- Lab7/Lab7/Lab07/MainActivity.cs
+++ Lab7/Lab7/Lab07/MainActivity.cs
@@ -12,7 +12,50 @@ namespace Lab07
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            var ValidarButton = FindViewById<Button>(Resource.Id.ValidarButton);
+            ValidarButton.Click += (sender, e) =>
+            {
+                Validar();
+            };
+        }
+
+        private async void Validar()
+        {
+            var client = new SALLab07.ServiceClient();
+
+            var EmailText = FindViewById<EditText>(Resource.Id.EmailText);
+            var PasswordText = FindViewById<EditText>(Resource.Id.PasswordText);
+            var myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+            var result = await client.ValidateAsync(EmailText.Text, PasswordText.Text, myDevice);
+            
+
+            if(Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var notificationText = $"{result.Status} {result.Fullname} {result.Token}";
+
+                var builder = new Notification.Builder(this)
+                    .SetContentTitle("Validacion de la Actividad")
+                    .SetContentText(notificationText)
+                    .SetSmallIcon(Resource.Drawable.Icon);
+
+                builder.SetCategory(Notification.CategoryMessage);
+
+                var ObjectNotifcation = builder.Build();
+                var Manager = GetSystemService(Android.Content.Context.NotificationService) as NotificationManager;
+                Manager.Notify(0, ObjectNotifcation);
+            }
+            else
+            {
+                var message = $"{result.Status}\n{result.Fullname}\n{result.Token}";
+
+                var resultText = FindViewById<TextView>(Resource.Id.ResultText);
+                resultText.Text = message;
+            }
+
+
         }
     }
 }
--- Lab7/Lab7/Lab07/Resources/Resource.Designer.cs
+++ Lab7/Lab7/Lab07/Resources/Resource.Designer.cs
@@ -57,6 +57,37 @@ namespace Lab07
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int EmailText = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int PasswordText = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int ResultText = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int ValidarButton = 2131034116;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
